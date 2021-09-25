b5f4f22aaa03f56d3a9dea650bdceb8321e38f6c
gdlyeabkov <gdlyeabkov@gmail.com>
2021-09-25 20:01:08 +0300

    init

---
+++ src/views/Beach.vue
@@ -0,0 +1,111 @@
+<template>
+  <div>
+    
+    <div class="beach">
+      
+      <div class="shortcut">
+      </div>
+      <div class="shortcut">
+      </div>
+      <div class="shortcut">
+      </div>
+      <div class="shortcut">
+      </div>
+      <div class="shortcut">
+      </div>
+      <div class="shortcut">
+      </div>
+      <div class="shortcut">
+      </div>
+      <div class="shortcut">
+      </div>
+      <div class="shortcut">
+      </div>
+      <div class="shortcut">
+      </div>
+      <div class="shortcut">
+      </div>
+      <div class="shortcut">
+      </div>
+      <div class="shortcut">
+      </div>
+      <div class="shortcut">
+      </div>
+      <div class="shortcut">
+      </div>
+      <div class="shortcut">
+      </div>
+      <div class="shortcut">
+      </div>
+      <div class="shortcut">
+      </div>
+      <div class="shortcut">
+      </div>
+      <div class="shortcut">
+      </div>
+      
+    </div>
+    
+    <div class="taskbar">
+      
+      <div class="taskbarIcons">
+        <div class="taskbarIcon">
+        </div>
+        <div class="taskbarIcon">
+        </div>
+        <div class="taskbarIcon">
+        </div>
+        <div class="taskbarIcon">
+        </div>
+        <div class="taskbarIcon">
+        </div>
+      </div>
+      
+      <div class="panelOfTools">
+        <div class="toolIcon">EN</div>
+        <span class="material-icons">
+          arrow_drop_up
+        </span>
+        <div class="toolIcon">
+          <span class="material-icons">
+            flag
+          </span>
+        </div>
+        <div style="toolIcon">
+          <span class="material-icons">
+            battery_full
+          </span>
+        </div>
+        <div class="toolIcon">
+          <span class="material-icons">
+            signal_cellular_alt
+          </span>
+        </div>
+        <div class="toolIcon">
+          <span class="material-icons">
+            volume_off
+          </span>
+        </div>
+        <div class="dateBlock">
+          <p class="dateBlockItem">
+            19:44
+          </p>
+          <p class="dateBlockItem">
+            25.09.2021
+          </p>
+        </div>
+      </div>
+      
+    </div>
+
+  </div>  
+</template>
+
+<script>
+export default {
+  name: 'Beach',
+  components: {
+    
+  }
+}
+</script>
